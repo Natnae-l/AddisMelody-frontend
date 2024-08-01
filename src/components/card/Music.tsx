b7eaf7e2e692f2431d667e7332b5d2b1ab80019a
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-08-01 15:24:02 +0300

    build added

--- src/components/card/Music.tsx
+++ src/components/card/Music.tsx
@@ -4,10 +4,18 @@ import Plus from "../../assets/plus.png";
 import { HorizontalContainer } from "../../styled /WelcomeStyled";
 import Banner from "../../assets/banner.png";
 import { Song } from "../../features/getSongSlice";
+import { useDispatch } from "react-redux";
+import { changeMusic } from "../../features/musicPlayerSlice";
 
-function Music({ banner, title, album, artist }: Song) {
+function Music({ banner, title, album, artist, audio }: Song) {
+  const dispatch = useDispatch();
   return (
-    <MusicContainer className="pointer box-hover">
+    <MusicContainer
+      className="pointer box-hover"
+      onClick={() =>
+        audio && dispatch(changeMusic({ music: audio, banner: banner || "" }))
+      }
+    >
       <HorizontalContainer $width="fit-content">
         <img src={banner || Banner} alt="" width="32px" height="32px" />
         <Paragraph $fontWeight={600} $fontSize="1rem">
--- src/components/home/Favourite.tsx
+++ src/components/home/Favourite.tsx
@@ -2,8 +2,25 @@ import { MainDiv } from "../../styled /Layout";
 import { Paragraph } from "../../styled /Text";
 import { DisplayGrid } from "../../styled /WelcomeStyled";
 import Music from "../card/Music";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { ThreeDot } from "react-loading-indicators";
 
 function Favourite() {
+  const musicState = useSelector((state: RootState) => state.songList);
+  const { songs, isLoading, error } = musicState;
+
+  const songRender = songs.map((song) => (
+    <Music
+      banner={song.banner}
+      title={song.title}
+      album={song.album}
+      artist={song.artist}
+      favourite={song.favourite}
+      audio={song.audio || ""}
+      genre={song.genre}
+    />
+  ));
   return (
     <MainDiv $flex={2.2} $radius="1.7rem" $fill="700px">
       <Paragraph
@@ -15,22 +32,34 @@ function Favourite() {
       >
         My favourite songs
       </Paragraph>
-      <DisplayGrid $padding="0px 0">
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
-        <Music />
+      <DisplayGrid
+        $justContent={isLoading ? "center !important" : "flex-start"}
+        $alignItems={isLoading ? "center !important" : "flex-start"}
+      >
+        {isLoading ? (
+          <div style={{ marginBlock: "100px" }}>
+            <ThreeDot />
+          </div>
+        ) : error != "" ? (
+          <div style={{ marginBlock: "100px", paddingInlineStart: "50px" }}>
+            <Paragraph $fontSize="2rem" $fontWeight={400} $align="center">
+              {error}
+            </Paragraph>
+          </div>
+        ) : !songRender.length ? (
+          <div style={{ marginBlock: "100px", paddingInlineStart: "50px" }}>
+            <Paragraph
+              $fontSize="2rem"
+              $fontWeight={400}
+              $align="center !important"
+              $color="#c9b7b7"
+            >
+              You don't have musics added
+            </Paragraph>
+          </div>
+        ) : (
+          songRender
+        )}
       </DisplayGrid>
     </MainDiv>
   );
